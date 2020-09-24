914361abd169c0656cc19065960e4a254b376518
Maria Filiptsova <m.filiptsova@gmail.com>
2020-09-24 18:25:54 +0200

    Initial commit

---
+++ onlineshop/main/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.forms import ModelChoiceField
+
+from .models import *
+
+
+class VagonkaAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='vagonka'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class TerraceAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='terrace'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Vagonka, VagonkaAdmin)
+admin.site.register(Terrace, TerraceAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
+admin.site.register(Order)
+
+
---
+++ onlineshop/main/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = (
+            'first_name', 'last_name', 'address', 'phone', 'buying_type',
+            'order_date', 'comment'
+        )
