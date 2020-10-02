59a75e1414bf6d5a78198ec5c2dfe018269f0957
Maria Filiptsova <m.filiptsova@gmail.com>
2020-10-02 15:50:00 +0200

    Add changes to admin

--- onlineshop/main/admin.py
+++ onlineshop/main/admin.py
@@ -20,13 +20,37 @@ class TerraceAdmin(admin.ModelAdmin):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
+class DoskaPolaAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='doska-pola'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class BrusAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='brus'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class StairsAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='stairs'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 admin.site.register(Category)
 admin.site.register(Vagonka, VagonkaAdmin)
 admin.site.register(Terrace, TerraceAdmin)
-admin.site.register(Stairs)
+admin.site.register(Stairs, StairsAdmin)
 admin.site.register(CartProduct)
 admin.site.register(Cart)
 admin.site.register(Customer)
 admin.site.register(Order)
-admin.site.register(DoskaPola)
-admin.site.register(Brus)
+admin.site.register(DoskaPola, DoskaPolaAdmin)
+admin.site.register(Brus, BrusAdmin)
